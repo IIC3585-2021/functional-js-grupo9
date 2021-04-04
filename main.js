973d1f99202fdc16442861b365f753f493128ac0
Agustín Peralta <agustinperalta@MacBook-Air-de-Agustin.local>
2021-04-04 02:42:04 -0400

    playgame

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const Player = (name) => {
           var x = array.map(element => whatToDo(element));
           score -= x.reduce((x, y) => (x + y));
           score = Math.abs(score);
+          console.log(name, score)
       },
       getName(){
           return name;
@@ -18,35 +19,37 @@ const Player = (name) => {
     }
  }
 
-const initGame = (...names) => {
+const initGame = (names) => {
     return names.map(name => Player(name));
 }
 
-const askJugada = () => {
-    
-}
-
-const game = function game(...names){
-    var players = initGame(...names);
+const game = (...names) => {
+    let plays = [['DB', [3,20], [3,19]], ['SB', [2,20], [3,20]], [[1, 18], [1, 10], [1, 12]]];
+    console.log(names)
+    var players = initGame(names);
     var currentPlayer = 0;
     return {
         playGame() {
             var curr = currentPlayer;
-            currentPlayer = currentPlayer + 1 === players.length() ? 0 : currentPlayer + 1;
-            var jugada = askJugada()
-            players[curr].play(jugada) == 0?terminar:playGame();
+            currentPlayer = currentPlayer + 1 === players.length ? 0 : currentPlayer + 1;
+            var jugada = plays.shift() // aca deberia ir el input de la jugada, esta hardcoded
+            players[curr].play(jugada) == 0?endGame():this.playGame();
         },
-    },
+        endGame() {
+            console.log('terminado')
+        }
+    }
 }
 
-const p1 = Player('p1')
-p1.play(['SB', [2,20], [3,20]])
-console.log(p1.getName())
-console.log(p1.getScore())
+// const p1 = Player('p1')
+// p1.play(['SB', [2,20], [3,20]])
+// console.log(p1.getName())
+// console.log(p1.getScore())
 
+const g = game('a','b')
+g.playGame()
+// const noFinish = (players) => (players.some((player) => player.getScore() === 0))?'juego termiando': noFinish; 
 
-const noFinish = (players) => (players.some((player) => player.getScore() === 0))?'juego termiando': noFinish; 
-
-const didWin = (player) => player.getScore() === 0 ? true : false
-const didFinish = (players) => players.reduce(player => didWin(player) && true );
-const play = (game, movements) => didFinish(game.getPlayers()) ? true : game.getPlayers().map(player => player.play)
+// const didWin = (player) => player.getScore() === 0 ? true : false
+// const didFinish = (players) => players.reduce(player => didWin(player) && true );
+// const play = (game, movements) => didFinish(game.getPlayers()) ? true : game.getPlayers().map(player => player.play)
